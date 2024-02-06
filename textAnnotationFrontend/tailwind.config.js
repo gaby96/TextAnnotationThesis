88cb488fb8ff4636cdfe563f92fc74f43e20a23f
GABRIEL ADDO <xaddo1@mendelu.cz>
2024-02-06 22:03:23 +0100

    added the create page vue page and made foreign keys null

--- textAnnotationFrontend/tailwind.config.js
+++ textAnnotationFrontend/tailwind.config.js
@@ -1,18 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./app.vue",
-    "./error.vue",
-  ],
-  theme: {
-    extend: {},
-    colors: {
-      'green': '#047857',
-    },
-  },
-  plugins: [],
-}
+export const content = [
+  "./components/**/*.{js,vue,ts}",
+  "./layouts/**/*.vue",
+  "./pages/**/*.vue",
+  "./plugins/**/*.{js,ts}",
+  "./app.vue",
+  "./error.vue",
+];
+export const theme = {
+  extend: {},
+  colors: {},
+};
+export const plugins = [];
