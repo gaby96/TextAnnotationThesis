582750a7df76936da5641b1c226ede9e385be60b
GABRIEL ADDO <xaddo1@mendelu.cz>
2024-02-08 09:49:17 +0100

    added components from Kristian's branch

---
+++ textAnnotationFrontend/pages/portal.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="">
+        <div class="w-[300px] flex flex-col justify-between  h-screen fixed py-5 px-2">
+            <div class="">
+                <img class="ml-3 mb-5" src="~/assets/Logo.png" style="height: 30px" alt="Annote Logo" loading="lazy" />
+                <UVerticalNavigation :links="links" />
+            </div>
+
+            <div class="">
+                <UVerticalNavigation :links="otherLinks" />
+            </div>
+        </div>
+        <div class=" w-full content ml-auto min-h-[100%] p-5">
+            <NuxtPage />
+        </div>
+    </div>
+</template>
+
+<script setup>
+definePageMeta({
+    redirect: '/portal/dashboard'
+})
+const links = [{
+    label: 'Dashboard',
+    icon: 'i-heroicons-home',
+    to: '/portal/dashboard'
+}]
+
+const otherLinks = [{
+    label: 'Settings',
+    icon: 'i-heroicons-chart-bar',
+    to: '/portal/settings'
+}, {
+    label: 'Logout',
+    icon: 'i-heroicons-arrow-right-on-rectangle-solid',
+    to: '/portal/logout',
+}]
+</script>
+
+<style scoped>
+.content {
+    width: calc(100% - 300px)
+}
+</style>
