125ea623b78140595f33776c73a438ae9f525f28
GABRIEL ADDO <xaddo1@mendelu.cz>
2024-07-13 23:05:30 +0200

    commented out langchain.py

--- backend/llmapp/lang_chain_config.py
+++ backend/llmapp/lang_chain_config.py
@@ -1,20 +1,18 @@
-from langchain_openai import OpenAI
-from django.conf import settings
-import json
-from typing import List
+# from langchain_openai import OpenAI
+# from django.conf import settings
+# import json
+# from typing import List
 
-# Initialize OpenAI API key
-openai_api_key = ""
-llm = OpenAI(api_key=openai_api_key)
+# llm = OpenAI
 
-def perform_ner(text):
-    prompt = (
-        "You are a highly intelligent and accurate Named-entity recognition(NER) system. "
-        "You take Passage as input and your task is to recognize and extract specific types of "
-        "named entities in that given passage and classify them into a set of entity types.\n"
-        f"Passage: {text}\n"
-        "Output the entities with their start and end offsets."
-    )
-    response = llm(prompt)
-    print(response)
-    return response
+# def perform_ner(text):
+#     prompt = (
+#         "You are a highly intelligent and accurate Named-entity recognition(NER) system. "
+#         "You take Passage as input and your task is to recognize and extract specific types of "
+#         "named entities in that given passage and classify them into a set of entity types.\n"
+#         f"Passage: {text}\n"
+#         "Output the entities with their start and end offsets."
+#     )
+#     response = llm(prompt)
+#     print(response)
+#     return response
